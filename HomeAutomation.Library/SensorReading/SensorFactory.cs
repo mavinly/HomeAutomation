cf6a51b58650ab33478a59b0574d51fd5b0a42b6
Chris Murphy <cmurph1624@gmail.com>
2015-09-02 20:33:26 -0600

    Adding business items for processing different sensors.

---
+++ HomeAutomation.Library/SensorReading/SensorFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.BusinessServices.SensorReading
+{
+    public enum Sensors
+        {
+            DoorSwitch,
+            Temperature
+        }
+    public class SensorFactory
+    {
+        public ISensorItem GetSensorItem(Sensors sensorName)
+        {
+            return new SensorItems.DoorSwitchItem();
+        }
+    }
+}
---
+++ HomeAutomation.Library/SensorReading/SensorItems/DoorSwitchItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAutomation.BusinessServices.SensorReading.SensorItems
+{
+    class DoorSwitchItem : ISensorItem
+    {
+        public void ProcessString(string reading)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
