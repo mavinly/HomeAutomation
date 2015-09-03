8dbbaa0ab209430722bbac88c8cb8948b5421997
Chris Murphy <cmurph1624@gmail.com>
2015-09-02 19:44:43 -0600

    Renaming Library Project

--- HomeAutomation.Library/IDoorSwitch.cs
+++ HomeAutomation.Library/IDoorSwitch.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeAutomation.Library
+namespace HomeAutomation.BusinessServices
 {
     interface IDoorSwitch
     {
