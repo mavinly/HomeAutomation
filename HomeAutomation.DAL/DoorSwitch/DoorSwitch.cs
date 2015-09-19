b7a4428647105c188b18cc9cb49fd8b96ea8e330
Chris Murphy <cmurph1624@gmail.com>
2015-09-19 15:36:20 -0600

    Adding file

---
+++ HomeAutomation.DAL/DoorSwitch/DoorSwitch.cs
@@ -0,0 +1,42 @@
+﻿using HomeAutomation.Model.DoorSwitchReading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeAutomation.DAL;
+
+namespace HomeAutomation.DAL.DoorSwitch
+{
+    public class DoorSwitch
+    {
+         List<DoorSwitchDTO>  _doorSwitchItems;
+
+        public DoorSwitch ()
+        {
+            if (_doorSwitchItems == null)
+            {
+                _doorSwitchItems = new List<DoorSwitchDTO>();
+            }
+        }        
+
+        public List<DoorSwitchDTO> GetReadings()
+        {
+            return _doorSwitchItems;
+            //using (var db = new HomeAutomation.DAL.HomeAutomationEntities())
+            //{
+            //    var query = (from item in db.SensorReadings
+            //                 join dss in db.DoorSwitchSensors on item.Id equals dss.SensorReadingID
+            //                 join sr in db.Sensors on item.SensorID equals sr.Id 
+            //                 select (new DoorSwitchDTO { ReadingDateTime = item.ReadingDateTime })).ToList();
+
+            //    return query;
+            //}
+        }
+
+        public void InsertReading(DoorSwitchDTO item)
+        {
+            _doorSwitchItems.Add(item);
+        }
+    }
+}
