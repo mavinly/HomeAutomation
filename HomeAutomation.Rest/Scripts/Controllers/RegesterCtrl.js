8b5dd81a2bf15089b7e383e2ce332f5b2e840b06
Chris Murphy <cmurph1624@gmail.com>
2015-10-21 20:05:40 -0600

    Adding Security

---
+++ HomeAutomation.Rest/Scripts/Controllers/RegesterCtrl.js
@@ -0,0 +1,12 @@
+﻿var myApp = angular.module('SecurityApp', []);
+
+myApp.controller('RegisterController', ['$scope', function ($scope) {
+    $scope.Email;
+    $scope.Password;
+    $scope.ConfirmPassword;
+
+    $scope.Register = function () {
+        var test = $scope.Email;
+        window.alert(test);
+    };
+}]);
