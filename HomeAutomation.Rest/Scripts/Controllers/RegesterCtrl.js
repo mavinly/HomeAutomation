8e0b928daa1629b677d5ff4d49e914a07dc70898
Chris Murphy <chris@mavinlydevelopment.com>
2015-11-10 20:06:08 -0700

    Changing how validation occurs

--- HomeAutomation.Rest/Scripts/Controllers/RegesterCtrl.js
+++ HomeAutomation.Rest/Scripts/Controllers/RegesterCtrl.js
@@ -56,4 +56,25 @@ myApp.controller('RegisterController', ['$scope', function ($scope) {
     {
         $scope.alerts.push({ type: "warning", msg: message});
     }
+
+    var compareTo = function() {
+    return {
+      require: "ngModel",
+      scope: {
+        otherModelValue: "=compareTo"
+      },
+      link: function(scope, element, attributes, ngModel) {
+
+        ngModel.$validators.compareTo = function(modelValue) {
+          return modelValue == scope.otherModelValue;
+        };
+
+        scope.$watch("otherModelValue", function() {
+          ngModel.$validate();
+        });
+      }
+    };
+  };
+
+  app.directive("compareTo", compareTo);
 }]);
